fadf1fe336454275f025ec73c83863279d9b6628
source-code888 <tafoyacabrera.antonio21@gmail.com>
2022-12-11 01:52:10 -0600

    Cambios

---
+++ src/main/java/main/model/UsuarioDAO.java
@@ -0,0 +1,27 @@
+package main.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+public class UsuarioDAO extends Conexion {
+
+    private QueryRunner QR = new QueryRunner();
+
+    public UsuarioDAO() {
+        super();
+    }
+
+    public List<Cuenta> clientes() {
+        List<Cuenta> cuentas = new ArrayList<>();
+        try {
+            cuentas = (List<Cuenta>) QR.query(getConn(), "SELECT * FROM cuenta",
+                    new BeanListHandler(Cuenta.class));
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        return cuentas;
+    }
+}
