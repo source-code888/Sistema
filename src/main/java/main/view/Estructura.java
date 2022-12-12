75e81dde5056ea4b3057f58f739a03e0436a0935
Heber Haziel Nava Martinez <l21380416@cdvictoria.tecnm.mx>
2022-12-12 14:09:06 -0600

    Menu de Ajustes completado

--- src/main/java/main/view/Estructura.java
+++ src/main/java/main/view/Estructura.java
@@ -1,12 +1,28 @@
 package main.view;
 
+import javax.swing.JMenuItem;
 import main.controller.EstructuraController;
 
 public class Estructura extends javax.swing.JFrame {
 
     public Estructura() {
         initComponents();
+       // inicializarMenu();
     }
+    
+    /*private void inicializarMenu(){
+        JMenuItem tienda = new JMenuItem("Tienda");
+        JMenuItem unidad = new JMenuItem("Unidad");
+        JMenuItem clasificacion = new JMenuItem("Clasificacion");
+        JMenuItem area = new JMenuItem("Area");
+        
+        popMenuAjustes.add(area);
+        popMenuAjustes.add(clasificacion);
+        popMenuAjustes.add(tienda);
+        popMenuAjustes.add(unidad);
+        System.out.println("BBBBBBB");
+        jMenuAjustes.setComponentPopupMenu(popMenuAjustes);
+    }*/
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -15,11 +31,15 @@ public class Estructura extends javax.swing.JFrame {
         panelMateriales = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
+        btnAgregarNuevoMaterial = new javax.swing.JButton();
         panelSalidas = new javax.swing.JPanel();
         panelEmpleados = new javax.swing.JPanel();
         jMenuBar = new javax.swing.JMenuBar();
         jMenuAjustes = new javax.swing.JMenu();
+        menuItemAreas = new javax.swing.JMenuItem();
+        menuItemClasificaciones = new javax.swing.JMenuItem();
+        menuItemTiendas = new javax.swing.JMenuItem();
+        menuItemUnidades = new javax.swing.JMenuItem();
         jMenuCerrar = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -61,7 +81,14 @@ public class Estructura extends javax.swing.JFrame {
             jTable1.getColumnModel().getColumn(5).setResizable(false);
         }
 
-        jButton1.setText("Agregar Nuevo Material");
+        EstructuraController controller = new EstructuraController(this, jMenuAjustes, jMenuCerrar);
+        btnAgregarNuevoMaterial.setText("Agregar Nuevo Material");
+        this.jMenuAjustes.addMouseListener(controller);
+        this.jMenuCerrar.addMouseListener(controller);
+        this.menuItemAreas.addActionListener(controller);
+        this.menuItemClasificaciones.addActionListener(controller);
+        this.menuItemTiendas.addActionListener(controller);
+        this.menuItemUnidades.addActionListener(controller);
 
         javax.swing.GroupLayout panelMaterialesLayout = new javax.swing.GroupLayout(panelMateriales);
         panelMateriales.setLayout(panelMaterialesLayout);
@@ -73,7 +100,7 @@ public class Estructura extends javax.swing.JFrame {
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 815, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMaterialesLayout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton1)))
+                        .addComponent(btnAgregarNuevoMaterial)))
                 .addContainerGap())
         );
         panelMaterialesLayout.setVerticalGroup(
@@ -81,8 +108,8 @@ public class Estructura extends javax.swing.JFrame {
             .addGroup(panelMaterialesLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 94, Short.MAX_VALUE)
+                .addComponent(btnAgregarNuevoMaterial)
                 .addContainerGap())
         );
 
@@ -96,7 +123,7 @@ public class Estructura extends javax.swing.JFrame {
         );
         panelSalidasLayout.setVerticalGroup(
             panelSalidasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 529, Short.MAX_VALUE)
+            .addGap(0, 556, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Salidas", panelSalidas);
@@ -109,16 +136,32 @@ public class Estructura extends javax.swing.JFrame {
         );
         panelEmpleadosLayout.setVerticalGroup(
             panelEmpleadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 529, Short.MAX_VALUE)
+            .addGap(0, 556, Short.MAX_VALUE)
         );
 
         tabbedPane.addTab("Empleados", panelEmpleados);
 
         jMenuAjustes.setText("Ajustes");
+
+        menuItemAreas.setText("Areas");
+        jMenuAjustes.add(menuItemAreas);
+
+        menuItemClasificaciones.setText("Clasificaciones");
+        jMenuAjustes.add(menuItemClasificaciones);
+
+        menuItemTiendas.setText("Tiendas");
+        menuItemTiendas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuItemTiendasActionPerformed(evt);
+            }
+        });
+        jMenuAjustes.add(menuItemTiendas);
+
+        menuItemUnidades.setText("Unidades");
+        jMenuAjustes.add(menuItemUnidades);
+
         jMenuBar.add(jMenuAjustes);
 
-        EstructuraController estructuraController = new EstructuraController(this, jMenuAjustes, jMenuCerrar);
-        this.jMenuCerrar.addActionListener(estructuraController);
         jMenuCerrar.setText("Cerrar");
         jMenuBar.add(jMenuCerrar);
 
@@ -138,13 +181,21 @@ public class Estructura extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void menuItemTiendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuItemTiendasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuItemTiendasActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnAgregarNuevoMaterial;
     private javax.swing.JMenu jMenuAjustes;
     private javax.swing.JMenuBar jMenuBar;
     private javax.swing.JMenu jMenuCerrar;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JMenuItem menuItemAreas;
+    private javax.swing.JMenuItem menuItemClasificaciones;
+    private javax.swing.JMenuItem menuItemTiendas;
+    private javax.swing.JMenuItem menuItemUnidades;
     private javax.swing.JPanel panelEmpleados;
     private javax.swing.JPanel panelMateriales;
     private javax.swing.JPanel panelSalidas;
