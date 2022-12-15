21838a2443b17b75fb2b1b74ff862a5ba22dcefd
source-code888 <tafoyacabrera.antonio21@gmail.com>
2022-12-14 18:19:50 -0600

    Revert "commit"
    This reverts commit e73bfd6ce70fed028d97555a732a09992fb66c6f.

--- src/main/java/main/Aplicacion.java
+++ src/main/java/main/Aplicacion.java
@@ -5,6 +5,9 @@ import main.view.Login;
 public class Aplicacion {
 
     public static void main(String[] args) {
+       //FUNCIONA
+       //Hola
+       //Hola Sem
        Login l = new Login();
        l.setResizable(false);
        l.setLocationRelativeTo(l);
