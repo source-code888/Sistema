e73bfd6ce70fed028d97555a732a09992fb66c6f
source-code888 <tafoyacabrera.antonio21@gmail.com>
2022-12-14 18:08:55 -0600

    commit

--- src/main/java/main/Aplicacion.java
+++ src/main/java/main/Aplicacion.java
@@ -5,9 +5,6 @@ import main.view.Login;
 public class Aplicacion {
 
     public static void main(String[] args) {
-       //FUNCIONA
-       //Hola
-       //Hola Sem
        Login l = new Login();
        l.setResizable(false);
        l.setLocationRelativeTo(l);
